7fe54dacaf772ed1d040c1fa9527c8b45f427073
dengqz <872114581@qq.com>
2018-06-22 23:27:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by cheese on 2018/6/22.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/")
+    public @ResponseBody String index(){
+        return "test";
+    }
+}
