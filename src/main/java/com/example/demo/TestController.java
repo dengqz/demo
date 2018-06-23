e84c8c14e81bb32b3f18445ed32019bb82ecf9e8
dengqz <872114581@qq.com>
2018-06-23 13:54:47 +0800

    di

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     @RequestMapping("/")
     public @ResponseBody String index(){
-        return "test12333990";
+        return "test1233399025";
     }
 }
